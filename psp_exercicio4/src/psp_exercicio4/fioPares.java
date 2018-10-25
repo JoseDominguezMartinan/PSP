3acc845afb302702df2dc84c8c0caac27b42f5cb
oracle <oracle@localhost>
2018-10-25 10:54:19 +0200

    exercicio 4 empezado

---
+++ psp_exercicio4/src/psp_exercicio4/fioPares.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psp_exercicio4;
+
+/**
+ *
+ * @author oracle
+ */
+public class fioPares extends Thread {
+    public fioPares(String name) {
+        super(name);
+    }
+    int j=0;
+    public void run(){
+        for(int i=0;i<=1000;i++){
+            if(i%2==0){
+                j=j+i;
+                        
+            }
+            System.out.println("a suma dos numeros pares é: "+j);
+        }
+        
+    }
+}
