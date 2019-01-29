dac49aa889d87e8cad20f74396ef26cd5dd4165c
Jose <Jose@DESKTOP-5N9473E>
2019-01-29 16:21:49 +0100

    empezado cilente e interfaz

---
+++ calculadora_v1.0/servidorCalculadora/src/servidorcalculadora/ServidorCalculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorcalculadora;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Jose
+ */
+public class ServidorCalculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        Conexion conexion=new Conexion();
+        conexion.crearConexion();
+        conexion.recibir();
+        conexion.enviar();
+    }
+    
+}
